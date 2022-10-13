2ae63344fde1fa35e7ae23c1f938086ff46eba2c
lordofthemind <supermanishkeshav98@gmail.com>
2022-10-13 21:26:01 +0530

    first

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello test" << endl;
+    cout << "Hello Manish" << endl;
+    cout << "Hello Manish" << endl;
+    cout << "Hello Manish" << endl;
+    cout << "Hello Manish" << endl;
+    cout << "Hello Manish" << endl;
+    cout << "Hello Manish" << endl;
+    cout << "Hello Manish" << endl;
+    cout << "Hello Manish" << endl;
+    cout << "Hello Manish" << endl;
+    return 0;
+}
