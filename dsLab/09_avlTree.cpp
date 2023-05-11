0994e828729604501fa24ceccb01323ef45e4fd4
Manish Kumar <supermanishkeshav98@gmail.com>
2023-05-11 17:09:54 +0530

    expts

--- dsLab/09_avlTree.cpp
+++ dsLab/09_avlTree.cpp
@@ -1,107 +1,116 @@
 // Avl tree with insertion and rotation
-
-#include<iostream>
+#include <iostream>
 using namespace std;
- 
-struct Node {
+
+struct Node
+{
     int key;
     Node *left;
     Node *right;
     int height;
 };
- 
-int height(Node *N) {
+
+int height(Node *N)
+{
     if (N == NULL)
         return 0;
     return N->height;
 }
- 
-Node* newNode(int key) {
-    Node* node = new Node;
+
+Node *newNode(int key)
+{
+    Node *node = new Node;
     node->key = key;
     node->left = NULL;
     node->right = NULL;
     node->height = 1;
-    return(node);
+    return (node);
 }
- 
-Node *rightRotate(Node *y) {
+
+Node *rightRotate(Node *y)
+{
     Node *x = y->left;
     Node *T2 = x->right;
- 
+
     x->right = y;
     y->left = T2;
- 
+
     y->height = max(height(y->left), height(y->right)) + 1;
     x->height = max(height(x->left), height(x->right)) + 1;
- 
+
     return x;
 }
- 
-Node *leftRotate(Node *x) {
+
+Node *leftRotate(Node *x)
+{
     Node *y = x->right;
     Node *T2 = y->left;
- 
+
     y->left = x;
     x->right = T2;
- 
+
     x->height = max(height(x->left), height(x->right)) + 1;
     y->height = max(height(y->left), height(y->right)) + 1;
- 
+
     return y;
 }
- 
-int getBalance(Node *N) {
+
+int getBalance(Node *N)
+{
     if (N == NULL)
         return 0;
     return height(N->left) - height(N->right);
 }
- 
-Node* insert(Node* node, int key) {
+
+Node *insert(Node *node, int key)
+{
     if (node == NULL)
-        return(newNode(key));
- 
+        return (newNode(key));
+
     if (key < node->key)
         node->left = insert(node->left, key);
     else if (key > node->key)
         node->right = insert(node->right, key);
     else
         return node;
- 
+
     node->height = 1 + max(height(node->left), height(node->right));
- 
+
     int balance = getBalance(node);
- 
+
     if (balance > 1 && key < node->left->key)
         return rightRotate(node);
- 
+
     if (balance < -1 && key > node->right->key)
         return leftRotate(node);
- 
-    if (balance > 1 && key > node->left->key) {
+
+    if (balance > 1 && key > node->left->key)
+    {
         node->left = leftRotate(node->left);
         return rightRotate(node);
     }
- 
-    if (balance < -1 && key < node->right->key) {
+    if (balance < -1 && key < node->right->key)
+    {
         node->right = rightRotate(node->right);
         return leftRotate(node);
     }
- 
     return node;
 }
- 
-void preOrder(Node *root) {
-    if(root != NULL) {
+
+void preOrder(Node *root)
+{
+    if (root != NULL)
+    {
         cout << root->key << " ";
         preOrder(root->left);
         preOrder(root->right);
     }
 }
- 
-int main() {
+
+int main()
+{
+
     Node *root = NULL;
- 
     root = insert(root, 5);
     root = insert(root, 3);
     root = insert(root, 2);
@@ -109,9 +118,9 @@ int main() {
     root = insert(root, 7);
     root = insert(root, 6);
     root = insert(root, 8);
- 
+
     cout << "Preorder traversal of the constructed AVL tree is: \n";
     preOrder(root);
- 
+
     return 0;
 }
